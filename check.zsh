ed13cfd78fbe0db0c89a6e253227f9f246dbd8de
Michael Komitee <mkomitee@gmail.com>
2015-04-21 23:28:41 -0400

    Fix files0-from check

--- check.zsh
+++ check.zsh
@@ -2,6 +2,6 @@
 set -x
 
 for bin in wc ./target/debug/wc; do
-    time echo -n "HELLO WORLD\nHELLO WORLD" | $bin -cmlLw - *(.) missing
-    time $bin --files0-from files0_from
+    # time echo -n "HELLO WORLD\nHELLO WORLD" | $bin -cmlLw - *(.) missing
+    find *(.) -print0 | time $bin --files0-from -
 done
